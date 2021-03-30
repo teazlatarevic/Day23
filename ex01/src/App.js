73ba55a10385c9c80db61037465f558a5087a498
Tea Zlatarevic <tea.zlatarevic@gmail.com>
2021-03-30 21:44:06 +0200

    Solution for the example 1

---
+++ ex01/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Plant from './Plant';
+
+function App() {
+  return (
+    <div id="App">
+      <Plant />
+    </div>
+  );
+}
+
+export default App;
